70ca4b3efdc22573ac0300e715be7805e37a9482
Robsonceia <114686103+Robsonceia@users.noreply.github.com>
2024-04-21 09:57:58 -0300

    Update User.js
    atualização do user

--- src/models/User.js
+++ src/models/User.js
@@ -6,4 +6,5 @@ const userSchema = new mongoose.Schema({
     senha: String,
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+module.exports = User;
